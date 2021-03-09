f08a6f8f4fc29f4b6441632d0bc6d7297e9dd730
atikah.abdullah <atikah_109@yahoo.com.my>
2021-03-09 19:52:16 +0800

    minor change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC15Fw4dwcf8zZDQtz11euft8c7xwy3oX0",
+    authDomain: "petroco-6c61d.firebaseapp.com",
+    projectId: "petroco-6c61d",
+    storageBucket: "petroco-6c61d.appspot.com",
+    messagingSenderId: "505032692564",
+    appId: "1:505032692564:web:94b4447f3cfb4a9c20eec1",
+    measurementId: "G-CV4G3PLBPR"
+  }
 };
